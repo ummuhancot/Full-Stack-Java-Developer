f1910527885a5bd402da82ab4edd0fcc500af6c6
ummuhanı çot <ummuhancot@gmail.com>
2025-09-06 19:28:02 +0300

    taslak hazırlanıyor

---
+++ src/components/common/spacer/Spacer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Spacer = ({height = 150}) => {
+  return (
+    <div style={{height:`${height}`}}></div>
+  )
+}
---
+++ src/services/config.js
@@ -0,0 +1,48 @@
+export const config = {
+  project: {
+    name: "FullStackJavaDeveloperLearn&Build",
+    slogon: "A developer’s journey: explore, try, learn, build",
+    description:
+      "Hi there! I’m a beginner Full Stack Developer. Here, I’m not just writing code, I’m learning, exploring, and sharing what I discover with others. This site is my personal learning journal, where you can find my course notes, experiences, and discoveries along the way.",
+  },
+  contact: {
+    info: {
+      address: {
+        value: "Cerrahpaşa, Etyemez Tekkesi Sk. No:2, 34098 Fatih/İstanbul",
+        icon: "pi pi-address-book",
+        link: "https://maps.app.goo.gl/P4KDPwkRSDArm7rJ6",
+      },
+    },
+    website: "https://javadevopslearn&build.com",
+    map: {
+      embed:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3010.916974054809!2d28.936392500000004!3d41.005190400000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cabbfadefcfbad%3A0x6d5fe027a28d1655!2sFatih%20Belediyesi%20Cerrahpa%C5%9Fa%20K%C3%BCt%C3%BCphanesi!5e0!3m2!1str!2str!4v1757175337732!5m2!1str!2str",
+    },
+    socialMedia: {
+      twitter: {
+        url: "https://x.com/ummuhan_cot",
+        icon: "pi pi-twitter",
+      },
+      instagram: {
+        url: "https://www.instagram.com/_wayward_and_angry_/",
+        icon: "pi pi-instagram",
+      },
+      facebook: {
+        url: "https://www.facebook.com/ummuhan.cot/",
+        icon: "pi pi-facebook",
+      },
+      linkedin: {
+        url: "https://www.linkedin.com/in/ummuhancot/",
+        icon: "pi pi-linkedin",
+      },
+      github: {
+        url: "https://github.com/ummuhancot",
+        icon: "pi pi-github",
+      },
+      youtube: {
+        url: "https://www.youtube.com/@ummuhancot",
+        icon: "pi pi-youtube",
+      },
+    },
+  },
+};
